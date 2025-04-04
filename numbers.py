85a850a006240d2aaa705c3643670fda0e52d94e
IgorSilva026 <igorssilva026@gmail.com>
2025-04-03 21:02:07 -0300

    feat: "att"

--- numbers.py
+++ numbers.py
@@ -1,12 +1,9 @@
 import random
 
 numeros = [3, 4, 7, 13, 19, 26, 31]
-
 x = 3
 
 numeros_escolhidos = random.sample(numeros, x)
-
-
 numeros_escolhidos.sort()
 
-print(f"Os seus numeros escolhidos foi: {numeros_escolhidos}")
+print(f"Os seus números escolhidos foram: {numeros_escolhidos}")
